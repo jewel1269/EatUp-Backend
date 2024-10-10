388612459df010564bf780d3ab9643e5fd434b85
Jewel Mia <145703213+jewel1269@users.noreply.github.com>
2024-10-10 20:58:15 +0600

    delte item with id

--- Routes/Cart/cart.js
+++ Routes/Cart/cart.js
@@ -60,6 +60,35 @@ router.post("/add", async (req, reply) => {
       reply.status(500).json({ message: "Server error while adding meal" });
     }
   });
+
+  //Item delete With id
+
+  router.delete("/meals/:id", async (req, res) => {
+    const { id } = req.params;
+    console.log(id);
+  
+    try {
+      const menuItem = await Cart.findById(id);
+      if (!menuItem) {
+        return res.status(404).json({ message: "Menu item not found" });
+      }
+  
+      // If quantity is greater than 1, decrease the quantity
+      if (menuItem.quantity > 1) {
+        menuItem.quantity -= 1; 
+        await menuItem.save(); 
+        return res.json({ message: "Quantity decreased successfully", item: menuItem });
+      } else {
+        
+        await Cart.deleteOne({ _id: id });
+        return res.json({ message: "Menu item deleted successfully" });
+      }
+    } catch (error) {
+      console.error("Error processing request:", error);
+      res.status(500).json({ message: "Server error" });
+    }
+  });
+  
   
 
 module.exports = router;
